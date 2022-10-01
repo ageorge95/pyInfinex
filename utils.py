6f933075eb130016755236799b6c38fb481471b3
Arteni George Daniel <arteni.george.daniel@gmail.com>
2022-10-01 16:35:25 +0300

    added utils.py

---
+++ utils.py
@@ -0,0 +1,6 @@
+def check_API_key(func):
+    def inner(*args, **kwargs):
+        if not args[0].API_key:
+            args[0]._log.error(f'{func.__name__} requires an API key !')
+        func(*args, **kwargs)
+    return inner
