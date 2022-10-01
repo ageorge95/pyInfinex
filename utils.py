b38ddf2ee64d985e6f2b932e36ac0b36b05688fe
Arteni George Daniel <arteni.george.daniel@gmail.com>
2022-10-01 17:40:23 +0300

    hotfix in utils.py

--- utils.py
+++ utils.py
@@ -2,5 +2,5 @@ def check_API_key(func):
     def inner(*args, **kwargs):
         if not args[0].API_key:
             args[0]._log.error(f'{func.__name__} requires an API key !')
-        func(*args, **kwargs)
+        return func(*args, **kwargs)
     return inner
