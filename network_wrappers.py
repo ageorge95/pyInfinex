8467259142ce9b061c2cd80a679575b72757096a
Arteni George Daniel <arteni.george.daniel@gmail.com>
2022-10-01 14:48:49 +0300

    fixed a typo in network_wrappers

--- network_wrappers.py
+++ network_wrappers.py
@@ -20,7 +20,7 @@ class API_call():
         current_retry = 0
         while current_retry < self.max_retries:
             try:
-                self._log.info(f'Sending an APOI call to {self.final_URL} at retry attempt {current_retry + 1}/{self.max_retries}')
+                self._log.info(f'Sending an API call to {self.final_URL} at retry attempt {current_retry + 1}/{self.max_retries}')
                 return {'API_call_success': True,
                         'data': get(self.final_URL,
                                     json=self.data).json()}
