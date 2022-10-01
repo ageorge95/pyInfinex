527595a94d97d0e298284ede72566fcbd9fc9e8f
Arteni George Daniel <arteni.george.daniel@gmail.com>
2022-10-01 13:43:40 +0300

    removed the base_endpoint as a configurable param

--- pYVayamos.py
+++ pYVayamos.py
@@ -5,17 +5,19 @@ from wallet import PublicWallet,\
 from spot import PublicSpot,\
     PrivateSpot
 
+base_endpoint: AnyStr = 'https://api.vayamos.cc'
+
 class pyVayamos(PublicWallet,
                 PrivateWallet,
                 PublicSpot,
                 PrivateSpot):
 
     def __init__(self,
-                 API_key: AnyStr = None,
-                 base_endpoint: AnyStr = 'https://api.vayamos.cc'):
+                 API_key: AnyStr = None):
 
         self._log = getLogger()
         self.API_key = API_key
+
         self.base_endpoint = base_endpoint
 
         super(pyVayamos, self).__init__()
